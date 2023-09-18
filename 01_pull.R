97177214b10b62892ee21dba555aa3e13832c321
riinuots <riinuots@gmail.com>
2023-09-18 11:16:33 +0100

    pins added

--- 01_pull.R
+++ 01_pull.R
@@ -15,12 +15,10 @@ patient_data_orig <- list("token"=Sys.getenv("gecko_data_collection_token"),
                           exportDataAccessGroups='false',
                           returnFormat='json') %>% 
   httr::POST(Sys.getenv("redcap_globalsurg_uri"), body = ., encode = "form") %>% 
-  httr::content()
-
-load("patient_data_orig.rda")
+  httr::content(guess_max = Inf)
 
 data = patient_data_orig
 source("GECKODataCollection-ArgonautDataPullRiin_R_2023-09-11_0813.r")
 patient_data_orig = data
-save(patient_data_orig, file = "patient_data_orig.rda")
+#save(patient_data_orig, file = "patient_data_orig.rda")
 rm(data)
