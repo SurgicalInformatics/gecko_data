d1e724aaea3fb60876da99cea2432a7634639978
riinuots <riinuots@gmail.com>
2024-03-06 13:45:24 +0000

    derived variables now have NA, rather than "Missing"

--- 04_cut_collapse.R
+++ 04_cut_collapse.R
@@ -1,3 +1,7 @@
+# library(tidyverse)
+# library(finalfit)
+# load("03_patient_data.rda")
+# 
 labels_keep = extract_variable_label(patient_data)
 
 patient_data = patient_data %>% 
@@ -8,12 +12,11 @@ patient_data = patient_data %>%
     # parse_number(as.character( is a bit stupid, but the column keeps flipping between
     # character and numeric based on whether people enter new mistakes/fix them on REDCap
     age_years = parse_number(as.character(age_years)),
-    age.groups = case_when(age_years < 18 ~ "Missing",
-                           between(age_years, 18, 30) ~ "18-30",
+    age.groups = case_when(between(age_years, 18, 30) ~ "18-30",
                            between(age_years, 31, 50) ~ "31-50",
                            between(age_years, 51, 70) ~ "51-70",
                            between(age_years, 71, 120) ~ "71+",
-                           TRUE ~ "Missing") %>% 
+                           TRUE ~ NA) %>% 
       ff_label("Age"),
     # n admissions ----
     admission_prior = parse_number(as.character(admission_prior)),
@@ -22,7 +25,7 @@ patient_data = patient_data %>%
                                        between(admission_prior, 2, 3) ~ "2-3",
                                        between(admission_prior, 5, 10) ~ "5-10",
                                        between(admission_prior, 11, 10000) ~ "10+",
-                                       TRUE ~ "Missing") %>% 
+                                       TRUE ~ NA) %>% 
       fct_relevel("None", "1", "2-3", "5-10", "10+") %>% 
       ff_label("Prior admissions"),
     # LOS ----
@@ -32,7 +35,7 @@ patient_data = patient_data %>%
                                   between(post30_los, 2, 3) ~ "2-3",
                                   between(post30_los, 5, 10) ~ "5-10",
                                   between(post30_los, 11, 10000) ~ "10+",
-                                  TRUE ~ "Missing") %>%
+                                  TRUE ~ NA) %>%
       fct_relevel("0", "1", "2-3", "5-10", "10+") %>% 
       ff_label("Length of Stay (days)"),
     # symptom onset diagnosis ----
@@ -44,7 +47,7 @@ patient_data = patient_data %>%
                                         between(pre_symp_adm_day, 15, 28) ~ "2-4 weeks",
                                         between(pre_symp_adm_day, 29, 182) ~ "1-6 months",
                                         between(pre_symp_adm_day, 182, 10000000) ~ "6+ months",
-                                        TRUE ~ "Missing") %>%
+                                        TRUE ~ NA) %>%
       fct_relevel("0 days", "1-2 days", "3-7 days", "1-2 weeks", "2-4 weeks", "1-6 months", "6+ months") %>% 
       ff_label("Symptom onset vs diagnosis (days)"),
     # diagnosis vs decision to operate ----
@@ -56,7 +59,7 @@ patient_data = patient_data %>%
                                         between(pre_diag_dec_day, 15, 28) ~ "2-4 weeks",
                                         between(pre_diag_dec_day, 29, 182) ~ "1-6 months",
                                         between(pre_diag_dec_day, 182, 10000000) ~ "6+ months",
-                                        TRUE ~ "Missing") %>%
+                                        TRUE ~ NA) %>%
       fct_relevel("0 days", "1-2 days", "3-7 days", "1-2 weeks", "2-4 weeks", "1-6 months", "6+ months") %>% 
       ff_label("Diagnosis vs decision to operate (days)"),
     # decision to operate vs operation ----
@@ -68,15 +71,12 @@ patient_data = patient_data %>%
                                       between(pre_dec_op_day, 15, 28) ~ "2-4 weeks",
                                       between(pre_dec_op_day, 29, 182) ~ "1-6 months",
                                       between(pre_dec_op_day, 182, 10000000) ~ "6+ months",
-                                      TRUE ~ "Missing") %>%
+                                      TRUE ~ NA) %>%
       fct_relevel("0 days", "1-2 days", "3-7 days", "1-2 weeks", "2-4 weeks", "1-6 months", "6+ months") %>% 
       ff_label("Decision to operate vs operation (days)"),
     # anaesthetic type ----
     op_anaes.grouped = op_anaes %>% 
-      str_replace("Total Intravenous Volatile Anaesthetic \\(TIVA\\)", "TIVA") %>% 
-      str_remove(" \\(e.g., midazolam\\)") %>% 
-      fct_na_value_to_level(level = "Missing") %>% 
-      fct_lump(8, other_level = "Other") %>% 
+      fct_lump(9, other_level = "Other combination") %>% 
       ff_label("Anaesthetic")
     # end ----
   ) %>% 
