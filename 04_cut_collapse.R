8c4314fc3f9b3d74f55c95ab8706ba6ddd874cbe
riinuots <riinuots@gmail.com>
2024-03-06 15:14:04 +0000

    add number of comorbidities, capitalisation typos in labelling script

--- 04_cut_collapse.R
+++ 04_cut_collapse.R
@@ -1,15 +1,32 @@
 # library(tidyverse)
 # library(finalfit)
 # load("03_patient_data.rda")
-# 
+
 labels_keep = extract_variable_label(patient_data)
 
 patient_data = patient_data %>% 
   #select(-redcap_data_access_group) %>% 
   mutate(ALL = factor("ALL"), .after = 1) %>% 
   mutate(
+    # co-morbidities ----
+    # these are separated by comma, listed within a single cell.
+    # so counting commas gives us total number.
+    # Have checked that no individual comorbidities have
+    # commas within themselves.
+    comorb_n = if_else(pt_comorbid == "None",
+                       0,
+                       str_count(pt_comorbid, ",") + 1) %>% 
+      ff_label("Number of comorbidities"),
+    comorb_n.groups = case_when(comorb_n == 0 ~ "None",
+                                comorb_n == 1 ~ "1",
+                                comorb_n == 2 ~ "2",
+                                comorb_n > 2  ~ "3+",
+                                .default = NA) %>% 
+      ff_label("Number of comorbidities"),
+    .after = pt_comorbid) %>% 
+  mutate(
     # age ----
-    # parse_number(as.character( is a bit stupid, but the column keeps flipping between
+    # parse_number(as.character()) is a bit stupid, but the column keeps flipping between
     # character and numeric based on whether people enter new mistakes/fix them on REDCap
     age_years = parse_number(as.character(age_years)),
     age.groups = case_when(between(age_years, 18, 30) ~ "18-30",
@@ -18,6 +35,8 @@ patient_data = patient_data %>%
                            between(age_years, 71, 120) ~ "71+",
                            TRUE ~ NA) %>% 
       ff_label("Age"),
+    .after = age_years) %>% 
+  mutate(
     # n admissions ----
     admission_prior = parse_number(as.character(admission_prior)),
     admission_prior.groups = case_when(admission_prior == 0 ~ "None",
@@ -28,6 +47,8 @@ patient_data = patient_data %>%
                                        TRUE ~ NA) %>% 
       fct_relevel("None", "1", "2-3", "5-10", "10+") %>% 
       ff_label("Prior admissions"),
+    .after = admission_prior) %>% 
+  mutate(
     # LOS ----
     post30_los = parse_number(as.character(post30_los)),
     post30_los.groups = case_when(post30_los == 0 ~ "0",
@@ -38,6 +59,8 @@ patient_data = patient_data %>%
                                   TRUE ~ NA) %>%
       fct_relevel("0", "1", "2-3", "5-10", "10+") %>% 
       ff_label("Length of Stay (days)"),
+    .after = post30_los) %>% 
+  mutate(
     # symptom onset diagnosis ----
     pre_symp_adm_day = parse_number(as.character(pre_symp_adm_day)),
     pre_symp_adm_day.groups = case_when(pre_symp_adm_day == 0 ~ "0 days",
@@ -50,6 +73,8 @@ patient_data = patient_data %>%
                                         TRUE ~ NA) %>%
       fct_relevel("0 days", "1-2 days", "3-7 days", "1-2 weeks", "2-4 weeks", "1-6 months", "6+ months") %>% 
       ff_label("Symptom onset vs diagnosis (days)"),
+    .after = pre_symp_adm_day) %>% 
+  mutate(
     # diagnosis vs decision to operate ----
     pre_diag_dec_day = parse_number(as.character(pre_diag_dec_day)),
     pre_diag_dec_day.groups = case_when(pre_diag_dec_day == 0 ~ "0 days",
@@ -62,6 +87,8 @@ patient_data = patient_data %>%
                                         TRUE ~ NA) %>%
       fct_relevel("0 days", "1-2 days", "3-7 days", "1-2 weeks", "2-4 weeks", "1-6 months", "6+ months") %>% 
       ff_label("Diagnosis vs decision to operate (days)"),
+    .after = pre_diag_dec_day) %>% 
+  mutate(
     # decision to operate vs operation ----
     pre_dec_op_day = parse_number(as.character(pre_dec_op_day)),
     pre_dec_op_day.groups = case_when(pre_dec_op_day == 0 ~ "0 days",
@@ -74,10 +101,11 @@ patient_data = patient_data %>%
                                       TRUE ~ NA) %>%
       fct_relevel("0 days", "1-2 days", "3-7 days", "1-2 weeks", "2-4 weeks", "1-6 months", "6+ months") %>% 
       ff_label("Decision to operate vs operation (days)"),
+    .after = pre_dec_op_day) %>% 
+  mutate(
     # anaesthetic type ----
     op_anaes.grouped = op_anaes %>% 
       fct_lump(9, other_level = "Other combination") %>% 
-      ff_label("Anaesthetic")
-    # end ----
-  ) %>% 
+      ff_label("Anaesthetic"),
+    .after = op_anaes) %>% 
   ff_relabel(labels_keep)
