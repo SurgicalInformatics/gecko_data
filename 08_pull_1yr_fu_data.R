fbd564bb3961bef6995da4834be03b4b68f09f9a
riinuots <riinuots@gmail.com>
2025-04-30 13:53:47 +0100

    factoring script from redcap

---
+++ 08_pull_1yr_fu_data.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+
+fu_data_orig <- list("token"=Sys.getenv("gecko_data_collection_token"),
+                          content='report',
+                          action='export',
+                          format='csv',
+                          type='flat',
+                          csvDelimiter='',
+                          report_id= 672,
+                          rawOrLabel='raw',
+                          rawOrLabelHeaders='raw',
+                          exportCheckboxLabel='false',
+                          exportSurveyFields='false',
+                          exportDataAccessGroups='false',
+                          returnFormat='json') %>%
+  httr::POST(Sys.getenv("redcap_globalsurg_uri"), body = ., encode = "form") %>%
+  httr::content(guess_max = Inf)
+
+save(fu_data_orig, file = "data_raw/gecko_redcap_pull_2025-04-30.rda")
+load("data_raw/gecko_redcap_pull_2025-04-30.rda")
+
+data = fu_data_orig
+source("redcap_factoring/GECKODataCollection-ArgonautDataPullRiin_R_2024-01-12_1324.r")
+
+fu_data_orig = data %>% 
+  mutate(internal_check = internal_check %>% 
+           factor() %>% 
+           fct_recode("Pass - included in analysis" = "1",
+                      "Period less than 90% complete" = "2",
+                      "Period missing - excluded from analysis" = "3")) %>% 
+  filter(internal_check == "Pass - included in analysis")
+
+#save(patient_data_orig, file = "patient_data_orig.rda")
+rm(data)
